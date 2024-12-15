3d9cf3f584ac88cc7296bbf76188e7bef40f3738
Георгий <sazykin.ga@gmail.com>
2024-12-15 14:32:42 +0300

    change insert_test_table.py v4

--- manuscript_py/insert_test_table.py
+++ manuscript_py/insert_test_table.py
@@ -4,7 +4,7 @@ from psycopg2 import sql
 try:
     conn = psycopg2.connect(host="85.192.35.124", port=5432, user="manuscript_2024", password="manuscript_ban_24", database="dbstud")
     cur = conn.cursor()
-    insert_query = sql.SQL("INSERT INTO a_symbol (key, value) VALUES ('some_text', '[{"street": "street address", "city": "Berlin"}]' )
+    insert_query = sql.SQL("INSERT INTO a_symbol (key, value) VALUES ('some_text', '[{}]' )")
     cur.execute(insert_query, (2,))  
     conn.commit()
     cur.close()
